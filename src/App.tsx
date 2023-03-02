71a749241ba686a670d074f213ed0fc942042de8
Wallysson <wlc.couto@gmail.com>
2023-03-02 20:20:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Header } from "./components/Header";
+
+export function App() {
+  return (
+    <div >
+      <Header />
+      <main>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, exercitationem? Totam rem nesciunt aspernatur incidunt. Repellat esse officia non placeat ex optio aperiam, ab nostrum odit amet in necessitatibus? Fugit.</div>
+        <img src="" alt="" />
+      </main>
+      <footer>
+        <p>&copy; 2023 StockMind. All rights reserved.</p>
+      </footer>
+    </div>
+  )
+}
+
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+export function Header() {
+  return (
+    <header>
+      <h1>Stock Mind</h1>
+      <nav>
+        <li>Home</li>
+        <li>News</li>
+        <li>Wallet</li>
+        <li>Stocks</li>
+        <li>Simulator</li>
+        <li>Login</li>
+      </nav>
+    </header>
+  );
+}
