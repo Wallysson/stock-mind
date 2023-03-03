e5d5513b32b7ca38f27a4212116037fbf3e4ea7a
Wallysson <wlc.couto@gmail.com>
2023-03-02 21:38:08 -0300

    feat(Header, App): base toggle menu and list of menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { Header } from "./components/Header";
 
 export function App() {
+  const menuItems = [
+    { label: "Home", url: "/" },
+    { label: "News", url: "/news" },
+    { label: "Wallet", url: "/wallet" },
+    { label: "Stocks", url: "/stocks" },
+    { label: "Simulator", url: "/simulator" },
+    { label: "Login", url: "/login" },
+  ];
+
+
   return (
-    <div >
-      <Header />
+    <div className="flex flex-col min-h-screen max-w-[1312px] mx-auto px-4">
+      <Header menuItems={menuItems}/>
       <main>
         <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, exercitationem? Totam rem nesciunt aspernatur incidunt. Repellat esse officia non placeat ex optio aperiam, ab nostrum odit amet in necessitatibus? Fugit.</div>
         <img src="" alt="" />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,49 @@
-export function Header() {
+import { Brain } from "phosphor-react";
+import { useState } from "react";
+
+interface MenuItem {
+  label: string;
+  url: string;
+}
+
+interface Props {
+  menuItems: MenuItem[];
+}
+
+export function Header({ menuItems }: Props) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu () {
+    setIsMenuOpen(!isMenuOpen)
+  } 
   return (
-    <header>
-      <h1>Stock Mind</h1>
-      <nav>
-        <li>Home</li>
-        <li>News</li>
-        <li>Wallet</li>
-        <li>Stocks</li>
-        <li>Simulator</li>
-        <li>Login</li>
-      </nav>
+    <header className="bg-gray-800 text-white px-4 py-3">
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-bold">Stock Website</h1>
+        <button
+          className="block sm:hidden focus:outline-none"
+          onClick={toggleMenu}
+        >
+          <svg
+            className="h-6 w-6 fill-current"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect y="4" width="24" height="2" />
+            <rect y="11" width="24" height="2" />
+            <rect y="18" width="24" height="2" />
+          </svg>
+        </button>
+        <nav className={`sm:flex ${isMenuOpen ? "block" : "hidden"}` }>
+          <ul className="flex gap-4">
+            {menuItems.map((menuItem, index) => (
+              <li className="hover:text-gray-100 focus:outline-none focus:text-gray-100 cursor-pointer" key={index}>
+                {menuItem.label}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
