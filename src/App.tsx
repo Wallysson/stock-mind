319bc38d9022fcc555bb6881d90f8c39fd3b1860
André Porto <112976026+andrevporto@users.noreply.github.com>
2023-03-03 04:48:07 -0300

    feat(Header, App, Footer): fix the toggle menu, adds bg image at landpage, create component footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import stonks from "./assets/stonks.jpg";
 
 export function App() {
   const menuItems = [
@@ -14,13 +16,15 @@ export function App() {
   return (
     <div className="flex flex-col min-h-screen max-w-[1312px] mx-auto px-4">
       <Header menuItems={menuItems}/>
-      <main>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, exercitationem? Totam rem nesciunt aspernatur incidunt. Repellat esse officia non placeat ex optio aperiam, ab nostrum odit amet in necessitatibus? Fugit.</div>
-        <img src="" alt="" />
+      <main className="flex flex-row items-center px-4">
+        <div
+          className="bg-cover bg-center bg-opacity-30 h-80 font-bold text-fuchsia-200 flex text-center items-center"
+          style={{ backgroundImage: `url(${stonks})` }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, exercitationem? Totam rem nesciunt aspernatur incidunt.
+        </div>
       </main>
-      <footer>
-        <p>&copy; 2023 StockMind. All rights reserved.</p>
-      </footer>
+      <Footer />
     </div>
   )
 }
